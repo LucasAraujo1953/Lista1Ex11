349ed7e6b1cd932ec1be82fe323fb7bfb9d249c3
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-13 15:41:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            double z;
+
+            Console.WriteLine("Precisamos calcular a operação: X^y");
+            Console.Write("Qual o valor de X: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Qual o valor de Y: ");
+            y = double.Parse(Console.ReadLine());
+
+            z = Math.Pow(x, y);
+
+            Console.WriteLine   ("O valor da operação X elevado a Y é {0}",z);
+
+        }
+    }
+}
